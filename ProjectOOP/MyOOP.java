ad3c5c225203183734639c0406e9bd4305613880
Hyeonseong Lee <hslee7231@gmail.com>
2022-08-04 22:14:31 +0900

    클래스 기본형식

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -1,34 +1,35 @@
 
 public class MyOOP {
-
-	public static String delimiter;	// 클래스 변수 선언
 	
 	public static void main(String[] args) {
-		delimiter = "----";	// 클래스 변수에 구분 문구 할당
-		printA();	// 메소드 호출
-		printA();
-		printB();
-		printB();
+		Print.delimiter = "----";	// Print 클래스 delimiter 변수에 구분 문구 할당
+		Print.A();	// Print 클래스의 A 메소드 호출
+		Print.A();
+		Print.B();	// Print 클래스의 B 메소드 호출
+		Print.B();
 		
-		delimiter = "****";	// 클래스 변수에 구분 문구 할당
-		printA();	// 메소드 호출
-		printA();
-		printB();
-		printB();
+		Print.delimiter = "****";	// Print 클래스 delimiter 변수에 구분 문구 할당
+		Print.A();	// Print 클래스의 A 메소드 호출
+		Print.A();
+		Print.B();	// Print 클래스의 B 메소드 호출
+		Print.B();
 	}
+}
 
-	private static void printA() {	// 반복적으로 사용되는 코드를 메소드로 그룹핑
+class Print {	// Print 클래스 선언
+	public static String delimiter;	// 클래스 변수 선언
+	
+	public static void A() {	// 반복적으로 사용되는 코드를 메소드로 그룹핑
 		//.. 일억줄의 코드
 		System.out.println(delimiter);	// 클래스 변수 출력
 		System.out.println("A");
 		System.out.println("A");
 	}
 	
-	private static void printB() {	// 매개 변수로 구분 문구를 받음
+	public static void B() {	// 매개 변수로 구분 문구를 받음
 		//.. 일억줄의 코드
 		System.out.println(delimiter);	// 클래스 변수 출력
 		System.out.println("B");
 		System.out.println("B");
 	}
-
 }
