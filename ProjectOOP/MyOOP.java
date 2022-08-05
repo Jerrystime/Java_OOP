0603057bdc7b1a032d2f0748e9ee43b2ccc40b07
Hyeonseong Lee <hslee7231@gmail.com>
2022-08-05 22:59:19 +0900

    생성자와 this

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -2,15 +2,13 @@
 public class MyOOP {
 	
 	public static void main(String[] args) {	// Java는 java 파일명과 똑같은 클래스의 main 메소드를 실행시키기로 약속이 되어있고, main 메소드를 가지는 클래스에만 앞에 public을 붙여줘야 한다.
-		Print p1 = new Print();	// Print 클래스를 데이터 타입으로 가지는 Print 인스턴스를 p1 변수에 담음
-		p1.delimiter = "----";	// p1 인스턴스 변수에 구분 문구 할당
+		Print p1 = new Print("----");	// Print 클래스를 데이터 타입으로 가지는 Print 인스턴스를 p1 변수에 담음
 		p1.A();	// p1 인스턴스 A() 인스턴스 메소드 호출
 		p1.A();
 		p1.B();	// p1 인스턴스 B() 인스턴스 메소드 호출
 		p1.B();
 		
-		Print p2 = new Print();	// Print 클래스를 데이터 타입으로 가지는 Print 인스턴스를 p1 변수에 담음
-		p2.delimiter = "****";
+		Print p2 = new Print("****");	// Print 클래스를 데이터 타입으로 가지는 Print 인스턴스를 p1 변수에 담음
 		p2.A();	// p2 인스턴스 A() 인스턴스 메소드 호출
 		p2.A();
 		p2.B();	// p2 인스턴스 B() 인스턴스 메소드 호출
--- ProjectOOP/Print.java
+++ ProjectOOP/Print.java
@@ -2,6 +2,10 @@
 class Print {	// class (클래스 이름) {} 안에 클래스의 멤버들을 담음
 	public String delimiter;	// 클래스 변수 선언
 	
+	public Print(String delimiter) {
+		this.delimiter = delimiter;
+	}
+	
 	public void A() {	// 반복적으로 사용되는 코드를 메소드로 그룹핑
 		//.. 일억줄의 코드
 		System.out.println(delimiter);	// 클래스 변수 출력
