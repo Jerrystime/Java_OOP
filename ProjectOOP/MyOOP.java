898e8554c243312025705db500b0f7fef741ee09
Hyeonseong Lee <hslee7231@gmail.com>
2022-08-05 19:50:22 +0900

    인스턴스

--- ProjectOOP/MyOOP.java
+++ ProjectOOP/MyOOP.java
@@ -2,18 +2,24 @@
 public class MyOOP {
 	
 	public static void main(String[] args) {	// Java는 java 파일명과 똑같은 클래스의 main 메소드를 실행시키기로 약속이 되어있고, main 메소드를 가지는 클래스에만 앞에 public을 붙여줘야 한다.
-		Print.delimiter = "----";	// Print 클래스 delimiter 변수에 구분 문구 할당
-		Print.A();	// Print 클래스의 A 메소드 호출
-		Print.A();
-		Print.B();	// Print 클래스의 B 메소드 호출
-		Print.B();
+		Print p1 = new Print();	// Print 클래스를 데이터 타입으로 가지는 Print 인스턴스를 p1 변수에 담음
+		p1.delimiter = "----";	// p1 인스턴스 변수에 구분 문구 할당
+		p1.A();	// p1 인스턴스 A() 인스턴스 메소드 호출
+		p1.A();
+		p1.B();	// p1 인스턴스 B() 인스턴스 메소드 호출
+		p1.B();
 		
-		Print.delimiter = "****";	// Print 클래스 delimiter 변수에 구분 문구 할당
-		Print.A();	// Print 클래스의 A 메소드 호출
-		Print.A();
-		Print.B();	// Print 클래스의 B 메소드 호출
-		Print.B();
+		Print p2 = new Print();	// Print 클래스를 데이터 타입으로 가지는 Print 인스턴스를 p1 변수에 담음
+		p2.delimiter = "****";
+		p2.A();	// p2 인스턴스 A() 인스턴스 메소드 호출
+		p2.A();
+		p2.B();	// p2 인스턴스 B() 인스턴스 메소드 호출
+		p2.B();
+		
+		p1.A();	// 인스턴스는 상태를 유지하고 있기 때문에 상태를 따로 초기화 시켜주지 않아도 된다.
+		p2.A();
+		p1.A();
+		p2.A();
+
 	}
 }
-
-// java 파일을 컴파일하면 각각의 클래스가 파일 형태로 만들어진다.
--- ProjectOOP/Print.java
+++ ProjectOOP/Print.java
@@ -1,15 +1,15 @@
 
 class Print {	// class (클래스 이름) {} 안에 클래스의 멤버들을 담음
-	public static String delimiter;	// 클래스 변수 선언
+	public String delimiter;	// 클래스 변수 선언
 	
-	public static void A() {	// 반복적으로 사용되는 코드를 메소드로 그룹핑
+	public void A() {	// 반복적으로 사용되는 코드를 메소드로 그룹핑
 		//.. 일억줄의 코드
 		System.out.println(delimiter);	// 클래스 변수 출력
 		System.out.println("A");
 		System.out.println("A");
 	}
 	
-	public static void B() {	// 매개 변수로 구분 문구를 받음
+	public void B() {	// 매개 변수로 구분 문구를 받음
 		//.. 일억줄의 코드
 		System.out.println(delimiter);	// 클래스 변수 출력
 		System.out.println("B");
